2fb5a43642ec5094f906e5ebe3a9408149855bcc
JonathanStevanus <jonathanstv1343@gmail.com>
2026-05-18 17:02:39 +0700

    .

--- notes/web/firebase-messaging-sw.js
+++ notes/web/firebase-messaging-sw.js
@@ -4,8 +4,8 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-comp
 // Gunakan konfigurasi web dari firebase_options.dart Anda
 firebase.initializeApp({
   apiKey: "AIzaSyAN...",
-  authDomain: "notes-5be35.firebaseapp.com",
-  projectId: "notes-5be35",
+  authDomain: "notes-a62d4.firebaseapp.com",
+  projectId: "notes-a62d4",
   storageBucket: "notes-5be35.firebasestorage.app",
   messagingSenderId: "1014103626721",
   appId: "1:1014103626721:web:f3b42914...",
